130de899d297afeb2f80812dc7e9ce388c2a18bd
Kunnar <kunnar.raiv@voco.ee>
2022-06-19 19:20:59 -0400

    yl3.sh

---
+++ praks4/yl3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#See skript arvutab nädala ajakulu
+echo Palun sisesta ainepunktide arv
+read ainepunktid
+echo Palun sisesta nädalate arv
+read nadal
+arvutus1=`echo $ainepunktid*26| bc`
+arvutus2=`echo "scale=1;$arvutus1/$nadal"| bc`
+umardamine="`echo  "($arvutus2+0.9)/1"| bc`"
+echo $umardamine
