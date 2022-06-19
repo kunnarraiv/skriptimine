d730c9ecb0fa65f6a7f59bf716cb90e545173d19
Kunnar <kunnar.raiv@voco.ee>
2022-06-19 19:19:53 -0400

    yl1.sh

---
+++ praks4/yl1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#See skript küsib kasutaja nime ja tervitab
+echo
+echo –n "Sisesta oma ees- ja perenimi: "
+read eesnimi perenimi
+echo "Sinu eesnimi on $eesnimi ja perenimi on $perenimi"
